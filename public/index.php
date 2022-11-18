c09dd62b7efe5ce7f420d2a5686ea42568d9474d
Nemanja Jocic <nemanja_jocic@yahoo.com>
2022-11-18 13:42:34 +0100

    phpinfo

--- public/index.php
+++ public/index.php
@@ -1,26 +1,4 @@
 <?php 
 require dirname(__DIR__) . '/vendor/autoload.php';
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\RequestOptions;
-
-const DEFAULT_ACCEPT_HEADER = 'application/json';
-    const DEFAULT_CACHE_HEADER = 'no-cache';
-
-$client = new GuzzleClient([
-    'base_uri' => null,
-    'timeout' => 0,
-    'allow_redirects' => false,
-]); 
-
-$response = $client->request('GET', 'http://filmapi.loopiarnd.com/items',
-            [
-                RequestOptions::HEADERS => [
-                    'Accept' => 'application/json',
-                    'X-Authorization' => 'Bearer username' . ":" . base64_encode('password')
-                ]
-            ]
-        );
-    
-var_dump(json_decode($response->getBody(), JSON_OBJECT_AS_ARRAY));
-exit;
+phpinfo();
 ?>
