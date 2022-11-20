b4f47f44ba7430b61df472d4bdde0cc099defe6a
Nemanja Jocic <nemanja_jocic@yahoo.com>
2022-11-20 21:46:43 +0100

    router

--- public/index.php
+++ public/index.php
@@ -1,4 +1,29 @@
-<?php 
-require dirname(__DIR__) . '/vendor/autoload.php';
-phpinfo();
+<?php
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\Classes\Items\HandlerItem;
+use App\Classes\Movies\Movies;
+use \App\Router\Router;
+$router = new Router();
+/**
+ * phpinfo();
+ */
+$router->get('/php-info', function() {
+    require_once dirname(__DIR__) . '/app/resources/phpinfo.php';
+});
+/**
+ * Movies;
+ */
+$router->get('/', Movies::class . "::movies");
+/**
+ * Get items
+ */
+$router->get('/v1/items', HandlerItem::class . "::exeItems");
+/**
+ * Get item
+ */
+$router->get('/v1/item', HandlerItem::class . "::exeItem");
+
+//router start
+$router->run();
 ?>
