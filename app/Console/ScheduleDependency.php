e956e2d0898437999c4ffb7e2d61a880a55680c7
Nemanja Jocic <nemanja_jocic@yahoo.com>
2022-11-23 12:18:07 +0100

    remove unss from ScheduleDependency

--- app/Console/ScheduleDependency.php
+++ app/Console/ScheduleDependency.php
@@ -2,30 +2,11 @@
 
 namespace App\Console;
 
-use \App\Console\Schedule;
 use \App\Classes\Items\ItemsCache;
 use \App\Classes\Items\ItemsGet;
 use \App\Classes\Items\ClientItems;
 use \App\Services\HttpService;
 use \App\Services\RedisService;
-use \App\Interface\RedisDependency;
-/**
- * init services dependencies
- */
-$http = new HttpService;
-$redis = new RedisService;
-/**
- * init classes dependencies
- */
-$client = new ClientItems($http);
-/**
- * process cache
- */
-$items = new ItemsGet($client);
-$itemsForCache = new ItemsCache($redis, $items);
-$classes = [
-    new ItemsCache($redis, $items)
-];
 
 class ScheduleDependency {
 
