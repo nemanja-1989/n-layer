a83abb916f9fa3e794aa70b6be6836321836a3b5
zeal12345 <nemanja.j@badsistems.com>
2022-11-29 12:27:04 +0100

    dependency on cache exists

--- app/Console/Schedule.php
+++ app/Console/Schedule.php
@@ -4,9 +4,24 @@ namespace App\Console;
 
 use App\Interface\FastCacheDependency;
 use \App\Interface\RedisDependency;
+use App\Services\FastCacheService;
+use App\Services\RedisService;
 
 class Schedule extends ScheduleDependency {
 
+    /**
+     * @param RedisService $redisService
+     * @param FastCacheService $fastCacheService
+     */
+    public function __construct
+    (
+        protected RedisService $redisService,
+        protected FastCacheService $fastCacheService,
+    )
+    {
+        $this->redisService = $redisService;
+        $this->fastCacheService = $fastCacheService;
+    }
     /**
      * @param RedisDependency $redisDependency
      * @return void
@@ -41,11 +56,29 @@ class Schedule extends ScheduleDependency {
         }
     }
 
+    private function checkRedisCache() :bool {
+        if($this->redisService->getService()->get('/v1/items') === null) {
+            return true;
+        }
+        return false;
+    }
+
+    private function checkFastCache() {
+        if($this->fastCacheService->getService()->get('/v1/items') === null) {
+            return true;
+        }
+        return false;
+    }
+
     /**
      * @return void
      */
     public function exe() :void {
-        $this->exeRedis();
-        $this->exeFastCache();
+        if($this->checkRedisCache() === false || $this->checkFastCache() === false) {
+            $this->exeRedis();
+            if($this->checkRedisCache() === false) {
+                $this->exeFastCache();
+            }
+        }
     }
 }   
--- kernel.php
+++ kernel.php
@@ -3,6 +3,8 @@
 require './vendor/autoload.php';
 
 use \App\Console\Schedule;
+use App\Services\FastCacheService;
+use App\Services\RedisService;
 
-$schedule = new Schedule();
+$schedule = new Schedule(new RedisService, new FastCacheService);
 $schedule->exe();
